ecfe047215f0370750b815dc11b85432bee5b31d
Esteban Velasquez <eavc.30@gmail.com>
2022-04-03 17:55:41 -0500

    feat(back): call code in an external package

--- hello/hello.go
+++ hello/hello.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	// Import code in an external package
+	"rsc.io/quote"
+)
 
 // main function prints "Hello, World"
 func main() {
 	fmt.Println("Hello, World!")
+
+	// Prints a quote from the Go programming language
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Opt())
 }
