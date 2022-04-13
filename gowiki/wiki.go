19f929e395e2578c96c6dce79efaf13fe429fdaa
Esteban Velasquez <eavc.30@gmail.com>
2022-04-13 07:31:05 -0500

    feat(back): add data structure and functions for gowiki

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -4,3 +4,33 @@ import (
 	"fmt"
 	"os"
 )
+
+// Page represents a wiki page
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+// save saves the Page's Body to a text file
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+// loadPage reads a text file and stores the body in the Page struct
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+// main is the entry point for the application
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
+}
