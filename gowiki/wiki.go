4a1a3608cbff8e317e1c2c82c5a364240e85f395
Esteban Velasquez <eavc.30@gmail.com>
2022-04-13 08:11:14 -0500

    feat(back): add editHandler function for gowiki

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -36,8 +36,30 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
+// editHandler handles requests to /edit/{title} for editing a page
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(
+		w,
+		`<h1>Editing %s</h1>
+        <form action="/save/%s" method="POST">
+            <textarea name="body">%s</textarea>
+            <br>
+            <input type="submit" value="Save">
+        </form>`,
+		p.Title,
+		p.Title,
+		p.Body,
+	)
+}
+
 // main is the entry point for the application
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
