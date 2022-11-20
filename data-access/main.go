0063d19a1b748aa6cb45ab28be76e7d37343077f
Esteban Velasquez <eavc.30@gmail.com>
2022-11-20 14:30:10 -0500

    feat(back): add connection to mysql database
    - added run script.

--- data-access/main.go
+++ data-access/main.go
@@ -2,10 +2,38 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
 
 func main() {
-	// ...
+	// capture the connection properties
+	cfg := mysql.Config{
+		User:                 os.Getenv("DB_USER"),
+		Passwd:               os.Getenv("DB_PASS"),
+		AllowNativePasswords: true,
+		Net:                  "tcp",
+		Addr:                 os.Getenv("DB_HOST"),
+		DBName:               os.Getenv("DB_NAME"),
+	}
+
+	// get a database handle
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatalf("Error opening database: %s", err)
+	}
+
+	// ping the database to verify the connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Error pinging database: %s", err)
+	}
+
+	fmt.Println("Successfully connected to database!")
 }
---
+++ data-access/run.sh
@@ -0,0 +1,6 @@
+export DB_USER=root
+export DB_PASS=password
+export DB_HOST="127.0.0.1:3306"
+export DB_NAME=recordings
+
+go run main.go
